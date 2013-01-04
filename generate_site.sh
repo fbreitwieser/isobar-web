c43c206f4d44618c8e8064c862f84237c7a7e05a
Florian Breitwieser <fbreitwieser@cemm.oeaw.ac.at>
2013-01-04 16:03:40 +0100

    site update

--- generate_site.sh
+++ generate_site.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 SECTION_ID=1
-sed -e "s/###ID###/$SECTION_ID/" template_header.html > index.html
-cat index-body.html >> index.html
-sed -e "s/###FOOTER###/Last update on `date +%Y-%m-%d`./" template_footer.html >>index.html
+sed -e "s/###ID###/$SECTION_ID/" templates/template_header.html > index.html
+cat templates/index-body.html >> index.html
+sed -e "s/###FOOTER###/Last update on `date +%Y-%m-%d`./" templates/template_footer.html >>index.html
 
 
 ./sweave2html isobar 2
@@ -11,8 +11,8 @@ sed -e "s/###FOOTER###/Last update on `date +%Y-%m-%d`./" template_footer.html >
 ./sweave2html isobar-ptm 3
 
 SECTION_ID=4
-sed -e "s/###ID###/$SECTION_ID/" template_header.html > references.html
-cat references-body.html >> references.html
-sed -e "s/###FOOTER###/Last update on `date +%Y-%m-%d`./" template_footer.html >>references.html
+sed -e "s/###ID###/$SECTION_ID/" templates/template_header.html > references.html
+cat templates/references-body.html >> references.html
+sed -e "s/###FOOTER###/Last update on `date +%Y-%m-%d`./" templates/template_footer.html >>references.html
 
 
